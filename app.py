915f0d5a84bb50cc05723f7aedce3ee40e368622
Teerapat Khunpech <engineerball@gmail.com>
2019-09-22 08:17:09 +0700

    update some code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
+system_name = os.environ.get("SYSNAME", "localhost")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Flask Dockerized this from %s' % system_name
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
